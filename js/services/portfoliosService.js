a75b3b5ea12be9be3603f7aa6ba3e6e46c9a5177
ablwr <ashley.blewer@gmail.com>
2014-09-10 00:02:17 -0400

    tech_used field added in data

--- js/services/portfoliosService.js
+++ js/services/portfoliosService.js
@@ -18,35 +18,40 @@ app.service('portfoliosService', function () {
             name: 'Flatiron Karaoke', 
             img: 'img/karaoke1.png',
             url: 'ablwr.github.io/flatiron-karaoke',
-            description: 'Popcorn.js app that syncs song lyrics and other media with video'
+            description: 'Popcorn.js app that syncs song lyrics and other media with video',
+            tech_used: 'Popcorn.js, Textillate.js'
         },
         {
             id: 2, 
             name: 'Scopitone', 
             img: 'img/scopitone.png',
             url: '#',
-            description: 'Randomly generates videos'
+            description: 'Randomly generates videos',
+            tech_used: 'YouTube API, CSS, Javascript'
         },
         {
             id: 3, 
             name: 'ffmproviser', 
             img: 'img/ffmproviser.png',
             url: 'http://ffmprovisor.herokuapp.com',
-            description: 'One-page guided web form that assists users in making FFMpeg commands'
+            description: 'One-page guided web form that assists users in making FFMpeg commands',
+            tech_used: 'Ruby, Rails, jQuery'
         },
         {
             id: 4, 
             name: 'Petsitfor.me', 
             img: 'img/petsitforme.png', 
             url: 'http://petsitfor.me',
-            description: 'Rails app to help dog walkers manage their business and facilitate interaction with clients'
+            description: 'Rails app to help dog walkers manage their business and facilitate interaction with clients',
+            tech_used: 'Ruby, Rails, jQuery'
         },
         {
             id: 5, 
             name: 'Need an Umbrella?', 
             img: 'img/umbrella.png', 
             url: 'http://needanumbrella.herokuapp.com',
-            description: 'Rails app for timely weather data'
+            description: 'Rails app for timely weather data',
+            tech_used: 'Ruby, Rails, Twilio API, Heroku Scheduler'
         }
 
     ];
