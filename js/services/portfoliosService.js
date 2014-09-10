b9c47d19989292891fd00f1a6be0cc0342b258bd
ablwr <ashley.blewer@gmail.com>
2014-09-09 23:54:59 -0400

    modal img links to active url

--- js/services/portfoliosService.js
+++ js/services/portfoliosService.js
@@ -16,31 +16,36 @@ app.service('portfoliosService', function () {
         {
             id: 1, 
             name: 'Flatiron Karaoke', 
-            img: 'img/karaoke1.png', 
+            img: 'img/karaoke1.png',
+            url: 'ablwr.github.io/flatiron-karaoke',
             description: 'Popcorn.js app that syncs song lyrics and other media with video'
         },
         {
             id: 2, 
             name: 'Scopitone', 
-            img: 'img/scopitone.png', 
+            img: 'img/scopitone.png',
+            url: '#',
             description: 'Randomly generates videos'
         },
         {
             id: 3, 
             name: 'ffmproviser', 
-            img: 'img/ffmproviser.png', 
+            img: 'img/ffmproviser.png',
+            url: 'http://ffmprovisor.herokuapp.com',
             description: 'One-page guided web form that assists users in making FFMpeg commands'
         },
         {
             id: 4, 
             name: 'Petsitfor.me', 
             img: 'img/petsitforme.png', 
+            url: 'http://petsitfor.me',
             description: 'Rails app to help dog walkers manage their business and facilitate interaction with clients'
         },
         {
             id: 5, 
             name: 'Need an Umbrella?', 
             img: 'img/umbrella.png', 
+            url: 'http://needanumbrella.herokuapp.com',
             description: 'Rails app for timely weather data'
         }
 
