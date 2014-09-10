ab1284372fbedb054a9756ea5f12dfe58443c46a
ablwr <ashley.blewer@gmail.com>
2014-09-09 20:43:33 -0400

    balanced but img source needs work

--- js/services/portfoliosService.js
+++ js/services/portfoliosService.js
@@ -14,10 +14,34 @@ app.service('portfoliosService', function () {
 
     var portfolios = [
         {
-            id: 1, name: 'Flatiron Karaoke', img: 'img/karaoke1.png', description: 'Kiss from a Rose...'
+            id: 1, 
+            name: 'Flatiron Karaoke', 
+            img: 'img/karaoke1.png', 
+            description: 'Popcorn.js app that syncs song lyrics and other media with video'
         },
         {
-            id: 2, name: 'Scopitone', img: 'img/scopitone.png', description: 'Something else'
+            id: 2, 
+            name: 'Scopitone', 
+            img: 'img/scopitone.png', 
+            description: 'Randomly generates videos'
+        },
+        {
+            id: 3, 
+            name: 'ffmproviser', 
+            img: 'img/ffmproviser.png', 
+            description: 'One-page guided web form that assists users in making FFMpeg commands'
+        },
+        {
+            id: 4, 
+            name: 'Petsitfor.me', 
+            img: 'img/petsitforme.png', 
+            description: 'Rails app to help dog walkers manage their business and facilitate interaction with clients'
+        },
+        {
+            id: 5, 
+            name: 'Need an Umbrella?', 
+            img: 'img/umbrella.png', 
+            description: 'Rails app for timely weather data'
         }
 
     ];
