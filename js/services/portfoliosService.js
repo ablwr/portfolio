e7bc60f36daa5895c5a38ee696a7627f75b43700
ablwr <ashley.blewer@gmail.com>
2014-09-11 18:21:33 -0400

    fixed typo and fixed typo

--- js/services/portfoliosService.js
+++ js/services/portfoliosService.js
@@ -19,6 +19,7 @@ app.service('portfoliosService', function () {
             img: 'img/karaoke1.png',
             url: 'ablwr.github.io/flatiron-karaoke',
             description: 'Popcorn.js app that syncs song lyrics and other media with video.',
+            longer_description: '',
             tech_used: 'Popcorn.js, Textillate.js'
         },
         {
@@ -27,14 +28,17 @@ app.service('portfoliosService', function () {
             img: 'img/scopitone.png',
             url: '#',
             description: 'Randomly generates videos from a playlist.',
+            longer_description: '',
             tech_used: 'YouTube API, CSS, Javascript'
         },
         {
             id: 3, 
             name: 'ffmproviser', 
-            img: 'img/ffmproviser.png',
+            img: 'img/ffmprovisor.png',
             url: 'http://ffmprovisor.herokuapp.com',
             description: 'One-page guided web form that assists users in making FFMpeg commands.',
+            longer_description: '+ Researched FFMpeg framework to create commands based on common user needs
++ Integrated a jQuery-heavy front end for seamless user experience',
             tech_used: 'Ruby, Rails, jQuery'
         },
         {
@@ -43,6 +47,10 @@ app.service('portfoliosService', function () {
             img: 'img/petsitforme.png', 
             url: 'http://petsitfor.me',
             description: 'Rails app to help dog walkers manage their business and facilitate interaction with clients.',
+            longer_description: '+ Implemented Devise for secure user authentication and sessions
++ Utilized CarrierWave for image upload and processing
++ Used the Twilio API to allow users to send text messages to other users
++ Developed complex database model with user-restricted layers of information access',
             tech_used: 'Ruby, Rails, jQuery'
         },
         {
@@ -51,6 +59,10 @@ app.service('portfoliosService', function () {
             img: 'img/umbrella.png', 
             url: 'http://needanumbrella.herokuapp.com',
             description: 'Rails app for timely weather data.',
+            longer_description: '+ Harnessed the Forecast.io API for gathering real-time user-specific weather data
++ Used the Twilio API to send text messages to users
++ Built Sidkiq workers to handle text message on sign up and Heroku scheduler for texting users according to their time preference
++ Implemented OAuth 1.1 to allow users to login through their Twitter account',
             tech_used: 'Ruby, Rails, Twilio API, Heroku Scheduler'
         }
 
